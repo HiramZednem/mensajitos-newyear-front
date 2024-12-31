e6c5d58692ee964968c83399f5c946bd4e26a9ae
Hiram Méndez <hirammendez000@gmail.com>
2024-12-31 01:51:20 -0600

    mensaje redireccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TarjetaComponent } from './tarjeta/tarjeta.component';
+import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
+  {path: '', component: ViewComponent},
   {path: ':uuid', component: TarjetaComponent},
 ];
 
